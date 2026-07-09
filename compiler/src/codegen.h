fffcb36ef0d2d8a97ed28f80e11813748e6f202b
robinpie <robin413@protonmail.com>
2026-07-09 17:03:19 -0500

    Create codegen.h

---
+++ compiler/src/codegen.h
@@ -0,0 +1,10 @@
+#ifndef OBOE_CODEGEN_H
+#define OBOE_CODEGEN_H
+
+#include "ast.h"
+#include <stdio.h>
+
+void codegen_set_source_dir(const char *dir);
+void codegen_program(Program *prog, FILE *out);
+
+#endif
